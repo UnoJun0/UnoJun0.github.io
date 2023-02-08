a34a6f30ba20393e55c05803102de6144c3db69e
UnoJun0 <juno@epochml.com>
2023-02-08 14:42:49 -0800

    added flags and generate text boxes for all languages

--- index.js
+++ index.js
@@ -1,17 +1,101 @@
+//English
 const btnEng = document.getElementById("generateEnglish");
-const out = document.getElementById("output");
+const outEng = document.getElementById("outputEnglish");
       
-btnEng.addEventListener("click", getWord);
+btnEng.addEventListener("click", getWordEng);
    
-function getWord(){      
-  var theWord= [
+function getWordEng(){      
+  var theWordEng= [
     'Hello',
     'Hi',
     'Yo',
   ];
 
-var wordNum = Math.floor(Math.random() * theWord.length);  
-  out.textContent = theWord[wordNum];
+var wordNum = Math.floor(Math.random() * theWordEng.length);  
+  outEng.textContent = theWordEng[wordNum];
   }
 
+//French
+const btnFre = document.getElementById("generateFrench");
+const outFre = document.getElementById("outputFrench");
+      
+btnFre.addEventListener("click", getWordFre);
+    
+function getWordFre(){      
+  var theWordFre= [
+    'Bonjoir',
+    'Salut',
+    'Bonsoir',
+  ];
 
+var wordNum = Math.floor(Math.random() * theWordFre.length);  
+  outFre.textContent = theWordFre[wordNum];
+  }
+
+//Japanese
+const btnJap = document.getElementById("generateJapanese");
+const outJap = document.getElementById("outputJapanese");
+    
+btnJap.addEventListener("click", getWordJap);
+  
+function getWordJap(){      
+var theWordJap= [
+  'Konnichiwa',
+  'Ohayou',
+  'Konbanwa',
+];
+
+var wordNum = Math.floor(Math.random() * theWordJap.length);  
+outJap.textContent = theWordJap[wordNum];
+}
+
+//German
+const btnGerm = document.getElementById("generateGerman");
+const outGerm = document.getElementById("outputGerman");
+    
+btnGerm.addEventListener("click", getWordGerman);
+  
+function getWordGerman(){      
+var theWordGerm= [
+  'Hallo',
+  'Guten Tag',
+  'Moin',
+];
+
+var wordNum = Math.floor(Math.random() * theWordGerm.length);  
+outGerm.textContent = theWordGerm[wordNum];
+}
+
+//Hindi
+const btnHindi = document.getElementById("generateHindi");
+const outHindi = document.getElementById("outputHindi");
+    
+btnHindi.addEventListener("click", getWordHindi);
+  
+function getWordHindi(){      
+var theWordHindi= [
+  'Namaste',
+  'Namaskar',
+  'Suprabhat',
+];
+
+var wordNum = Math.floor(Math.random() * theWordHindi.length);  
+outHindi.textContent = theWordHindi[wordNum];
+}
+
+//Chinese
+const btnChinese = document.getElementById("generateChinese");
+const outChinese = document.getElementById("outputChinese");
+    
+btnChinese.addEventListener("click", getWordChinese);
+  
+function getWordChinese(){      
+var theWordChinese= [
+  'Nihao',
+  'Ninhao',
+  'Dajiahao',
+];
+
+var wordNum = Math.floor(Math.random() * theWordChinese.length);  
+outChinese.textContent = theWordChinese[wordNum];
+}
